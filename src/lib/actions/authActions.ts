3216c52197a70a7e7079c9325798b04ecf4f5444
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-03-05 22:57:07 +0100

    encrypting URL in emails with JWT

--- src/lib/actions/authActions.ts
+++ src/lib/actions/authActions.ts
@@ -1,9 +1,10 @@
 "use server";
 
 import { User } from "@prisma/client";
-import prisma from "../prisma";
+import prisma from "@/lib/prisma";
 import * as bcrypt from "bcrypt";
 import { compileActivationTemplate, sendEmail } from "../mail";
+import { signJwt, verifyJwt } from "../jwt";
 
 export async function registerUser(
   user: Omit<User, "id" | "emailVerified" | "image">
@@ -15,8 +16,30 @@ export async function registerUser(
     },
   });
 
-  const activationUrl = `${process.env.NEXTAUTH_URL}/auth/activation/${result.id}`;
+  const jwtUserId = signJwt({ id: result.id });
+  const activationUrl = `${process.env.NEXTAUTH_URL}/auth/activation/${jwtUserId}`;
   const body = compileActivationTemplate(user.firstName, activationUrl);
   await sendEmail({ to: user.email, subject: "Activate your account", body });
   return result;
 }
+
+type ActivateUserFunc = (
+  jwtUserId: string
+) => Promise<"userNotExist" | "alreadyActivated" | "success">;
+
+export const activateUser: ActivateUserFunc = async (jwtUserID) => {
+  const payload = verifyJwt(jwtUserID);
+  const userId = payload?.id;
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+  if (!user) return "userNotExist";
+  if (user.emailVerified) return "alreadyActivated";
+  const result = await prisma.user.update({
+    where: { id: userId },
+    data: {
+      emailVerified: new Date(),
+    },
+  });
+  return "success";
+};
